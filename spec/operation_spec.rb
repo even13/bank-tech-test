5b00e474a66bdbbf2ea82632c48197da070ebef1
Eve Noirault <student@Makerss-MacBook-Air.local>
2020-01-27 15:38:40 +0000

    test: operation debit

--- spec/operation_spec.rb
+++ spec/operation_spec.rb
@@ -11,4 +11,13 @@ describe Operation do
       expect(operation.date).to eq Time.now
     end
   end
+
+  it "debits the user's account by 10" do
+    Timecop.freeze do
+      operation = Operation.new(-10, 90)
+      expect(operation.amount).to eq(-10)
+      expect(operation.balance).to eq 90
+      expect(operation.date).to eq Time.now
+    end
+  end
 end
