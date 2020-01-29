63cf0224323c357197a6f78cd01056336085ff88
Eve Noirault <student@Makerss-MacBook-Air.local>
2020-01-29 18:13:23 +0000

    fix: independent testing of classes

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -4,11 +4,12 @@ require 'timecop'
 describe Account do
   let(:statement) { double :statement }
   let(:operation) { double :operation }
-  subject(:account) { Account.new(operation) }
+  subject(:account) { Account.new(operation, statement) }
 
   describe "#deposit" do
     it "a client should be able to deposit money" do
       allow(operation).to receive(:log) { [[Time.now.strftime("%d/%m/%Y"), "1000.00", '', "1000.00"]] }
+      allow(statement).to receive(:print_statement) { "date || credit || debit || balance\n" + "#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00\n" }
       allow(operation).to receive(:entry).with(1000, 1000, :credit)
       account.deposit(1000)
       expect { account.statement }.to output("date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00\n").to_stdout
@@ -18,14 +19,13 @@ describe Account do
   describe "#withdraw" do
     it "a client should be able to withdraw money" do
       Timecop.freeze do
-        allow(operation).to receive(:log) { [[Time.now.strftime("%d/%m/%Y"), "1000.00", '', "1000.00"], [Time.now.strftime("%d/%m/%Y"), '', "200.00", "800.00"]] }
+        allow(operation).to receive(:log) { [[Time.now.strftime("%d/%m/%Y"), "1000.00", '', "1000.00"], [Time.now.strftime("%d/%m/%Y"), '', "300.00", "700.00"]] }
+        allow(statement).to receive(:print_statement) { "date || credit || debit || balance\n" + "#{Time.now.strftime("%d/%m/%Y")} || || 300.00 || 700.00\n" + "#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00\n" }
         allow(operation).to receive(:entry).with(1000, 1000, :credit)
         allow(operation).to receive(:entry).with(200, 800, :debit)
         account.deposit(1000)
-        p account.statement, "here"
         account.withdraw(200)
-        p account.statement, "two"
-        expect { account.statement }.to output("date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y")} || || 200.00 || 800.00\n#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00\n").to_stdout
+        expect { account.statement }.to output("date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y")} || || 300.00 || 700.00\n#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00\n").to_stdout
       end
     end
 
@@ -37,12 +37,14 @@ describe Account do
   describe "#statement" do
     it "a client cannot print their account statement without operations" do
       allow(operation).to receive(:log) { [] }
+      allow(statement).to receive(:print_statement).and_raise("No operations to display yet")
       expect { account.statement }.to raise_error("No operations to display yet")
     end
 
     it "a client will see their operations displayed on the statement" do
       Timecop.freeze do
         allow(operation).to receive(:log) { [[Time.now.strftime("%d/%m/%Y"), "1000.00", '', "1000.00"]] }
+        allow(statement).to receive(:print_statement) { "date || credit || debit || balance\n" + "#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00\n" }
         allow(operation).to receive(:entry).with(1000, 1000, :credit)
         account.deposit(1000)
         expect { account.statement }.to output("date || credit || debit || balance\n#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00\n").to_stdout
